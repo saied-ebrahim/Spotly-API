a850c7e85e11c7aee607760ae93c67ff7eb4ae50
Omar1030 <omarabdelmoaty816@gmail.com>
2025-11-28 11:15:14 +0200

    Validation for login and signup

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { signUpController, loginController, refreshTokenController } from "../controllers/auth-controller.js";
+import validateMiddleware from "../middlewares/validation-middleware.js";
+import { loginSchema, signUpSchema } from "../validations/auth-validation.js";
 
 const router = express.Router();
 
-router.post("/signUp", signUpController);
-router.post("/login", loginController);
+router.post("/signUp", validateMiddleware(signUpSchema), signUpController);
+router.post("/login", validateMiddleware(loginSchema), loginController);
 router.post("/refreshToken", refreshTokenController);
 
 export default router;
